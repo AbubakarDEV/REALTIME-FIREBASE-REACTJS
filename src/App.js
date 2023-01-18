a39ce09c0cc3bf58beea9f120b02cb0da986f634
Abubakar Jilani <abub1156@gmail.com>
2023-01-18 18:08:01 +0500

    create design using material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import Button from "@mui/material/Button";
 function App() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch(
+      "https://fir-realtime-reactjs-2e885-default-rtdb.asia-southeast1.firebasedatabase.app/simpleForm.json",
+      {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        // mode: "cors", // no-cors, *cors, same-origin
+        // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // redirect: "follow", // manual, *follow, error
+        // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify(formData), // body data type must match "Content-Type" header
+      }
+    );
+    res.json(); // parses JSON response into native JavaScript objects
+    if (res.statusText == "OK") {
+      alert("Data Inserted Successfully");
+      setFormData({
+        username: "",
+        password: "",
+      });
+    } else {
+      alert("Something went wrong");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="conatiner">
+      <TextField
+        label="Username"
+        onChange={handleChange}
+        name="username"
+        value={formData.username}
+        className="textfieldd"
+      />
+
+      <TextField
+        label="Password"
+        name="password"
+        onChange={handleChange}
+        className="textfieldd"
+        value={formData.password}
+      />
+      <Button variant="contained" onClick={handleSubmit}>
+        Submit
+      </Button>
     </div>
   );
 }
